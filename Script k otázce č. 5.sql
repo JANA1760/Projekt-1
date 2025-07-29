a5d220bb35fd52ad130c92a843db6419de951ca1
JANA1760 <jana.kunovska@volny.cz>
2025-07-29 15:00:38 +0200

    Update Script k otázce č. 5.sql

--- Script k otázce č. 5.sql
+++ Script k otázce č. 5.sql
@@ -6,12 +6,13 @@ SELECT
 country,
 population,
 year,
-gdp,
+round (gdp)AS gdp,
 gini,
 round (gdp/population)AS gdp_head,
-taxes,
+round (taxes)AS danove_zatizeni,
 median_age_2018
 FROM t_jana_sindelkova_project_sql_secondary_final tjspssf
-WHERE country IN ('Albania','Greece','Germany','Spain','Austria',
+WHERE country IN ('Albania','Germany','Spain','Austria',
 'Slovakia', 'Italy', 'Norway', 'Bulgaria','Serbia', 'Slovenia')
-AND YEAR IN ('2011','2012','2013','2014', '2015','2016','2017','2018', '2019', '2020');
+AND YEAR IN ('2016','2017','2018','2019', '2020')
+ORDER BY  YEAR, country;
