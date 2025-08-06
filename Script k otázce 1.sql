eaf49addff735c8e2f73a6299987ce7efecc7014
JANA1760 <jana.kunovska@volny.cz>
2025-08-06 16:21:16 +0200

    Update Script k otázce 1.sql

--- Script k otázce 1.sql
+++ Script k otázce 1.sql
@@ -1,47 +1,30 @@
 Otázka č. 1
- Rostou v průběhu let mzdy ve všech odvětvích, 
-nebo v některých klesají?
+Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
 
- - výpis z tabulky PRIMARY_final
- - seskupit dle odvětví a dle let
-  - potřebuji jedno odvětví, a v průběhu let
- 
- návod: 
- - co potřebuji vypsat,
- - z čeho,
- - seskupení, řazení;
- 
-VYtvořeno VIEW, vybráno - rok, odvětví, mzda
+návod: 
+- co potřebuji vypsat,
+- z čeho,
+- seskupení, řazení.
 
- CREATE VIEW v_mzdy_odvetvi AS 
- SELECT
- DISTINCT tprimary.payroll_year  AS rok,
- tprimary.industry AS odvetvi,
- tprimary.average_wages AS prumerna_mzda
- FROM t_jana_sindelkova_project_sql_primary_final AS tprimary
- GROUP BY industry, payroll_year, average_wages;
+komentář:
+- vytvořeno VIEW, vybráno - rok, odvětví, mzda,
+- z uvedeného VIEW filtrujeme min, max, avg seskupíme, seřadíme a máme.
 
-Z výše uvedené VIEW filtrujeme min, max, avg seskupíme, seřadíme a máme
+kód:
+CREATE VIEW v_mzdy_odvetvi AS 
+SELECT
+DISTINCT tprimary.payroll_year  AS rok,
+tprimary.industry AS odvetvi,
+tprimary.average_wages AS prumerna_mzda
+FROM t_jana_sindelkova_project_sql_primary_final AS tprimary
+GROUP BY industry, payroll_year, average_wages;
 
 SELECT 
- rok,
- odvetvi,
- min(prumerna_mzda),
- max(prumerna_mzda),
-round (avg (prumerna_mzda))
+rok,
+odvetvi,
+min(prumerna_mzda) AS min_mzda,
+max(prumerna_mzda) AS max_mzda,
+round (avg (prumerna_mzda)) AS prumerna_mzda
 FROM v_mzdy_odvetvi
 GROUP BY odvetvi,rok
-ORDER BY odvetvi ASC, rok asc;
-
-
-
- 
-
-
- 
- 
- 
- 
-
- 
- 
+ORDER BY odvetvi ASC, rok ASC;
