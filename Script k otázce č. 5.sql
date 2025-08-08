c3cef3cc4d9f0ed609d2af635c6e5252c9334e7b
JANA1760 <jana.kunovska@volny.cz>
2025-08-08 21:16:57 +0200

    Update Script k otázce č. 5.sql

--- Script k otázce č. 5.sql
+++ Script k otázce č. 5.sql
@@ -3,16 +3,16 @@ pokud HDP vzroste výrazněji v jednom roce, projeví se to na cenách potravin 
 ve stejném nebo následujícím roce výraznějším růstem?
 
 SELECT
-country,
-population,
-year,
-round (gdp)AS gdp,
-gini,
-round (gdp/population)AS gdp_head,
+country AS country,
+population AS population,
+year AS rok, 
+round (gdp) AS gdp,
+gini AS gini_index,
+round (gdp/population) AS gdp_head,
 round (taxes) AS danove_zatizeni,
-median_age_2018
+median_age_2018 AS stredni_vek
 FROM t_jana_sindelkova_project_sql_secondary_final
 WHERE country IN ('Albania','Germany','Spain','Austria',
 'Slovakia', 'Italy', 'Norway', 'Bulgaria','Serbia', 'Slovenia')
 AND YEAR IN ('2016','2017','2018','2019', '2020')
-ORDER BY  year, country;
+ORDER BY   country, rok;
