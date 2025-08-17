5d190ffdee28490a41b338409c4d546d161e2ecb
JANA1760 <jana.kunovska@volny.cz>
2025-08-17 12:42:53 +0200

    Update Script k otázce č. 2.sql

--- Script k otázce č. 2.sql
+++ Script k otázce č. 2.sql
@@ -17,23 +17,6 @@ kometář:
 - původní tabulku t_kupni_sila_chleb_mleko jsem odstranila a nahradila, 
 - vytvořila jsem novou tabulku t_kupni_sila_chleb_mleko_bez_rozliseni.
 
-kód:
-
-CREATE TABLE t_kupni_sila_chleb_mleko AS
-SELECT
-food_category,
-round (avg (price)) AS average_price, 
-payroll_year AS "year",
-industry,
-round (avg (average_wages))AS average_payroll,
-round (avg (average_wages)/ avg (price)) AS kupni_sila
-FROM t_jana_sindelkova_project_sql_primary_final
-WHERE payroll_year IN ('2006','2018')
-AND food_category IN('Chléb konzumní kmínový','Mléko polotučné pasterované')
-GROUP BY food_category, payroll_year, industry
-ORDER BY payroll_year;
-
-DROP TABLE IF EXISTS t_kupni_sila_chleb_mleko;
 
 upravený kód - bez dělení na odvětví :
 
